2b9ac29c4e33ccd59a63921e434e64fa005ac28b
Lowe Thiderman <lowe.thiderman@gmail.com>
2016-08-04 17:28:24 +0200

    Remove pointless test

--- web_test.go
+++ web_test.go
@@ -1,29 +1,29 @@
 package main
 
-import (
-	"github.com/stretchr/testify/assert"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-	"strings"
-	"testing"
-)
+// import (
+// 	"github.com/stretchr/testify/assert"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"strings"
+// 	"testing"
+// )
 
-func testServer() *httptest.Server {
-	db := MockDatabase()
-	s := NewServer(db)
-	r := s.BuildRouter()
-	return httptest.NewServer(r)
-}
+// func testServer() *httptest.Server {
+// 	db := MockDatabase()
+// 	s := NewServer(db)
+// 	r := s.BuildRouter()
+// 	return httptest.NewServer(r)
+// }
 
-func TestServeIndexHtml(t *testing.T) {
-	assert := assert.New(t)
-	s := testServer()
-	defer s.Close()
+// func TestServeIndexHtml(t *testing.T) {
+// 	assert := assert.New(t)
+// 	s := testServer()
+// 	defer s.Close()
 
-	res, err := http.Get(s.URL)
-	html, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	assert.Nil(err)
-	assert.True(strings.Contains(string(html), "<!doctype html>"))
-}
+// 	res, err := http.Get(s.URL)
+// 	html, err := ioutil.ReadAll(res.Body)
+// 	res.Body.Close()
+// 	assert.Nil(err)
+// 	assert.True(strings.Contains(string(html), "<!doctype html>"))
+// }
