db14471203bb4ced0fc3ac24c4df617605a96b6f
Lowe Thiderman <lowe.thiderman@gmail.com>
2016-08-09 11:28:21 +0200

    Add dev.sh helper script
    Closes #1

---
+++ dev.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+cd $(dirname $(readlink -f $0))
+
+# Check that we have the things that we need to run this
+error=false
+for x in go npm nginx; do
+    if ! type $x &> /dev/null ; then
+        echo "$x not found. Please install."
+        error=true
+    fi
+done
+
+if $error ; then
+    exit 1
+fi
+
+function start_nginx() {
+    mkdir -p logs
+    # TODO: Make sure we can run this without sudo
+    sudo nginx -p . -c conf/nginx.conf
+}
+
+function start_api() {
+    go build -v || exit $?
+    ./towerfall
+}
+
+function start_npm() {
+    cd js/
+    if [[ ! -d "node_modules" ]]; then
+        echo "No modules found - will download."
+        npm install
+    fi
+
+    npm run dev
+}
+
+case "$1" in
+    api)
+        start_api
+        ;;
+    nginx)
+        start_nginx
+        ;;
+    npm)
+        start_npm
+        ;;
+    *)
+        echo "usage: dev.sh [towerfall|nginx|npm]"
+        exit 1
+esac
