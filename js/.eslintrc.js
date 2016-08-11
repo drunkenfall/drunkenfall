458f9bf902cd5e2bb590f14590c4a563087bfa07
Lowe Thiderman <lowe.thiderman@gmail.com>
2016-08-12 00:18:23 +0200

    Relax eslint settings

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -12,6 +12,10 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    // allow commas, becuase not doing so is stupid
+    'comma-dangle': 0,
+    // don't care about quotes yo
+    'quotes': 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
