454296c7d708dbfe69d1435d6adc31f7dd3e3529
Lowe Thiderman <lowe.thiderman@gmail.com>
2016-08-12 00:17:35 +0200

    Make websocket messages type agnostic

--- websockets/message.go
+++ websockets/message.go
@@ -2,15 +2,10 @@ package websockets
 
 // Message is the data to send back
 type Message struct {
-	Author string `json:"author"`
-	Body   string `json:"body"`
+	Data interface{} `json:"data"`
 }
 
 // Ping is a simple ping message
 type Ping struct {
 	P int `json:"p"`
-}
-
-func (m *Message) String() string {
-	return m.Author + " says " + m.Body
 }
