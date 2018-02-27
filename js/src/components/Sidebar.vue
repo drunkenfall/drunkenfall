9dd156c2e818ba9af3c5cd2caf6512da3ada1aa5
Lowe Thiderman <lowe.thiderman@gmail.com>
2018-02-28 00:15:25 +0100

    Rewrite the sidebar to be fully responsive

--- js/src/components/Sidebar.vue
+++ js/src/components/Sidebar.vue
@@ -1,67 +1,35 @@
 <template>
-  <div v-if="showSidebar">
-    <div id="sidebar">
-      <router-link :to="dispatchLink">
-        <div class="logo">
-          <img alt="" src="/static/img/oem.svg"/>
-        </div>
+<div v-if="showSidebar">
+  <div id="sidebar" :class="{visible: hamburgerActive}">
+    <router-link :to="dispatchLink" class="logo">
+      <div>
+        <img alt="" src="/static/img/oem.svg"/>
+      </div>
+    </router-link>
+
+    <div @click="toggle" class="hamburger">
+      <icon name="bars" />
+    </div>
+
+    <div class="blocks" @click="toggle" :class="{visible: hamburgerActive}">
+      <router-link class="action" :to="{ name: 'tournaments'}">
+        <div class="icon">🔥</div>
+        <p>Tournaments</p>
       </router-link>
 
-      <div class="content main">
-        <div class="blocks">
-          <router-link class="action"
-            :to="{ name: 'tournaments'}">
-            <div class="icon">
-              🔥
-            </div>
-            <p>Tournaments</p>
-            <div class="clear"></div>
-          </router-link>
+      <router-link class="action" :to="{ name: 'archers'}">
+        <div class="icon">🏹</div>
+        <p>Archers</p>
+      </router-link>
 
-          <router-link class="action"
-            :to="{ name: 'archers'}">
+      <router-link v-if="user.isProducer" class="action" :to="{ name: 'admin'}">
+        <div class="icon">💪</div>
+        <p>Superpowers</p>
+      </router-link>
+    </div>
 
-            <div class="icon">
-              🏹
-            </div>
-            <p>Archers</p>
-            <div class="clear"></div>
-          </router-link>
-
-          <router-link v-if="user.isProducer" class="action"
-            :to="{ name: 'admin'}">
-            <div class="icon">
-              💪
-            </div>
-            <p>Superpowers</p>
-            <div class="clear"></div>
-          </router-link>
-        </div>
-
-        <div v-if="user.isProducer && viewing(['tournaments']) && false">
-          <h1>Actions</h1>
-          <div class="actions links">
-            <router-link class="action"
-              :to="{ name: 'new'}">
-              <div class="icon positive">
-                <icon name="plus"></icon>
-              </div>
-              <p>New tournament</p>
-              <div class="clear"></div>
-            </router-link>
-
-            <a @click="clear">
-              <div class="icon danger">
-                <icon name="trash"></icon>
-              </div>
-              <p>Clear test</p>
-              <div class="clear"></div>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div v-if="userLoaded && user.authenticated" class="user">
+    <div v-if="userLoaded && user.authenticated" class="user">
+      <div class="controls">
         <div @click="settings" class="settings">
           <icon name="cog"></icon>
         </div>
@@ -70,25 +38,26 @@
         <div @click="logout" class="logout">
           <icon name="sign-out"></icon>
         </div>
-
-        <h1 id="sidebar-username" :class="user.color">{{user.nick}}</h1>
       </div>
 
-      <div v-if="!viewing(['facebook']) && userLoaded && !user.authenticated" class="content facebook">
-        <div class="links">
-          <a href="/api/facebook/login">
-            <div class="icon">
-              <icon name="facebook"></icon>
-            </div>
-            <p>Sign in</p>
-            <div class="clear"></div>
-          </a>
-        </div>
-      </div>
-
-      <div class="clear"></div>
+      <h1 id="sidebar-username" :class="user.color">{{user.nick}}</h1>
     </div>
+
+    <div v-if="!viewing(['facebook']) && userLoaded && !user.authenticated" class="content facebook">
+      <div class="links">
+        <a href="/api/facebook/login">
+          <div class="icon">
+            <icon name="facebook"></icon>
+          </div>
+          <p>Sign in</p>
+          <div class="clear"></div>
+        </a>
+      </div>
+    </div>
+
+    <div class="clear"></div>
   </div>
+</div>
 </template>
 
 <script>
@@ -103,7 +72,16 @@ export default {
     Person,
   },
 
+  data () {
+    return {
+      hamburgerActive: false,
+    }
+  },
+
   methods: {
+    toggle () {
+      this.hamburgerActive = !this.hamburgerActive
+    },
     viewing (names) {
       // Returns true if currently viewing any of the route names.
       return _.includes(names, this.$route.name)
@@ -166,39 +144,10 @@ export default {
   z-index: 100;
   font-size: 1.3em;
 
-  .blocks .action {
-    @include headline();
-    transition: 0.2s;
-    text-shadow: 2px 2px 3px rgba(0,0,0,0.3);
-    color: #888;
-    display: block;
-    width: 100%;
-    text-align: center;
-    /* background-color: rgba(0,0,0,0.2); */
-    /* border-bottom: 1px solid rgba(0,0,0,0.1); */
-    /* border-top: 1px solid rgba(255,255,255,0.1); */
-    padding: 3rem 0;
-    box-shadow: none;
-
-    &.router-link-active {
-      background-color: rgba(0,0,0,0.2);
-      color: $fg-default;
-      .icon {
-        color: $accent;
-      }
-    }
-
-    .icon {
-      font-size: 3em;
-      text-shadow: 3px 3px 5px rgba(0,0,0,0.5);
-    }
-    p {
-      margin-top: 1em;
-      font-weight: 100;
-      font-size: 0.8em;
-      text-transform: uppercase;
-    }
-  }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: space-between;
 
   /* Real devices */
   @media screen and ($desktop: $desktop-width) {
@@ -210,14 +159,52 @@ export default {
     font-size: 1.3em;
     width: $sidebar-width;
 
+    .hamburger {
+      display: none;
+    }
+
     .logo {
       padding: 1em;
       width: 50%;
       margin: 0px auto;
     }
 
-    >.content {
-      /* margin: 1.5rem; */
+    .blocks {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      flex-grow: 1;
+
+      .action {
+        @include headline();
+        transition: 0.2s;
+        text-shadow: 2px 2px 3px rgba(0,0,0,0.3);
+        color: #888;
+
+        width: 100%;
+        text-align: center;
+        padding: 2rem 0;
+        box-shadow: none;
+
+        &.router-link-active {
+          background-color: rgba(0,0,0,0.2);
+          color: $fg-default;
+          .icon {
+            color: $accent;
+          }
+        }
+
+        .icon {
+          font-size: 3em;
+          text-shadow: 3px 3px 5px rgba(0,0,0,0.5);
+        }
+        p {
+          margin-top: 1em;
+          font-weight: 100;
+          font-size: 0.8em;
+          text-transform: uppercase;
+        }
+      }
     }
   }
 
@@ -225,20 +212,61 @@ export default {
   @media screen and ($device: $device-width) {
     position: relative;
     width: 100%;
-    height: 5em;
+    min-height: 3em;
+    transform: 0.3s;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+    .visible {
+      min-height: 5em;
+    }
+
+    .logo, .hamburger, .user {
+      width: 33%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 0.5em 0;
+    }
+
+    .hamburger {
+      font-size: 1.5em;
+      order: 1;
+      cursor: pointer;
+    }
 
     .logo {
-      /* width: 40%; */
-      height: 100%;
-      float: left;
-      display: block;
-      margin: 0px auto;
+      order: 2;
+
+      img {
+        margin: 0 auto;
+        height: 2em;
+      }
     }
 
-    >.content.main {
+    .user {
+      order: 3;
+    }
+
+    .blocks {
+      order: 4;
+      transform: 0.3s;
       display: none;
-    }
+      opacity: 0;
 
+      &.visible {
+        opacity: 1;
+        display: flex;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+
+        .action {
+          flex-grow: 1;
+          text-align: center;
+        }
+      }
+    }
   }
 
   h1 {
@@ -251,11 +279,16 @@ export default {
 
   @media screen and ($desktop: $desktop-width) {
     .user {
+      margin-top: 1em;
       height: 140px;
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: 0;
+      display: flex;
+      flex-direction: column;
+
+      .controls {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
 
       img {
         height: 75px;
@@ -268,14 +301,20 @@ export default {
         display: block;
         cursor: pointer;
       }
+
       h1 {
         transition: 0.5s;
         font-size: 1.5rem;
+        width: 100%;
       }
-      div {
-        position: absolute;
-        top: 30px;
+
+      .settings, .logout {
         height: 1.3em;
+        display: flex;
+        flex-grow: 1;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
 
         color: #999;
         filter: drop-shadow(2px 2px 3px rgba(0,0,0,0.5));
@@ -285,13 +324,6 @@ export default {
         &:hover {
           cursor: pointer;
           color: $fg-default;
-        }
-
-        &.settings {
-          left: 30px;
-        }
-        &.logout {
-          right: 30px;
         }
       }
     }
@@ -311,27 +343,18 @@ export default {
 
   @media screen and ($device: $device-width) {
     .user {
-      float: right;
-      width: 30%;
-
       img {
-        position: absolute;
-        right: 5%;
-        top: 50%;
-        transform: translateY(-50%);
         cursor: pointer;
-
-        height: 60%;
+        height: 2em;
         object-fit: cover;
         border-radius: 100%;
         box-shadow: $shadow-default;
         background-color: rgba(10,12,14,0.3);
-        display: block;
       }
       h1 {
         display: none;
       }
-      div {
+      .settings, .logout {
         display: none;
       }
     }
